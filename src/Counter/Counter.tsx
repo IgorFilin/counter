2d07ad9931268e184c51e2309def0e2aa3a6cf77
Igor <filinigor@yandex.ru>
2022-07-16 17:08:58 +0300

    Refactoring Counter

---
+++ src/Counter/Counter.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Display} from "./Display/Display";
+import {Button} from "../Button/Button";
+import s from './Counter.module.css'
+
+type CounterTypeProps = {
+    count: number
+    addCount: () => void
+    resetCount: () => void
+    isMaxValueError: boolean
+    isMinValueError: boolean
+}
+
+export const Counter: React.FC<CounterTypeProps> = ({
+                                                        count,
+                                                        addCount,
+                                                        resetCount,
+                                                        isMaxValueError,
+                                                        isMinValueError
+                                                    }) => {
+
+
+    return (
+        <div className={s.container}>
+            <div className={s.content}>
+                <Display count={count} isMaxValueError={isMaxValueError}/>
+                <div className={s.buttons}>
+                    <Button onClick={addCount} disabled={isMaxValueError}>increment value</Button>
+                    <Button className={s.buttonReset} onClick={resetCount} disabled={isMinValueError}>res</Button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
--- src/Counter/Display/Display.tsx
+++ src/Counter/Display/Display.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import s from './Display.module.css'
 
-export const Counter = () => {
+type DisplayTypeProps = {
+    count: number
+    isMaxValueError: boolean
+}
+
+export const Display: React.FC<DisplayTypeProps> = ({count, isMaxValueError}) => {
     return (
-        <div>
-
+        <div className={isMaxValueError ? s.error : s.display}>
+            {count}
         </div>
     );
 };
